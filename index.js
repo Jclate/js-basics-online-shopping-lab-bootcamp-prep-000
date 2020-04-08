e7e88eeeb412bfd325787d6e7bdb9ac7ea549547
Jake Gonchar <jake098877@gmail.com>
2020-04-08 20:29:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -132,11 +132,12 @@ if (item === cart[i].itemName) {
   var item = cart[i].itemName
   cart.splice(i,1)
 }
+
+return cart
 else {
   return 'That item is not in your cart.'
 }
 }
-return cart
 }
 
 function placeOrder(cardNumber) {
